3e256ebc7dced6bdc1053af68df67cbce0737301
Amiel Martin <amiel@carnesmedia.com>
2012-07-07 02:11:57 -0700

    Generate Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kimballpairs::Application.config.secret_token = 'e7217432eb3d92ba3f53013aa8fff7783b92781f43c33d8e17774e8242b52b74279f2eb83b61ac451dcdd3f8373258959b551a3aed6dd1f75c2dc31405f7da57'
