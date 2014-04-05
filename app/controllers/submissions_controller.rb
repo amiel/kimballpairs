4ac89fc02888a94d9fc0fffb58c8d1e42a6b5615
Amiel Martin <amiel@carnesmedia.com>
2014-04-04 18:20:51 -0800

    Fix email link; add flash for submissions

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -9,6 +9,7 @@ class SubmissionsController < ApplicationController
   def create
     @submission = Submission.create(params[:submission])
     @submission.notification_email.deliver
+    flash[:alert] = "Your submission has been sent. If it is approved, it will appear on this website"
     respond_with @submission, location: root_path
   end
 
